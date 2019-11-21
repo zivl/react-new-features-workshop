3f0f6b095ac8787eaceb30c52eae22c43cecf227
Ziv Levy <levi.ziv@gmail.com>
2019-11-21 23:15:23 +0200

    add inital notification api

---
+++ react-workshop/src/services/notifications.js
@@ -0,0 +1,7 @@
+export const notifications = {
+    notifyParent,
+    // notify parent on missing child
+    // notify parent on general issue such as "bring a new blanket", "kid has kinim"
+    notifyDayCareManager,
+    notifyDayCareStaff,
+}
