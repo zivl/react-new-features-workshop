38b3842643da26f6a813158107e09e62f91c366c
Ziv Levy <levi.ziv@gmail.com>
2020-01-02 17:38:40 +0200

    add child controller

---
+++ src/components/Controllers/AddChildController.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AddChildController = () => {
+    return (
+        <button type="button" class="btn btn-info">Add Child</button>
+    );
+};
+
+export default AddChildController;
