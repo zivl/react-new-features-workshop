a47efd23ee2f85911a1c236486b59454caf381bb
Ziv Levy <levi.ziv@gmail.com>
2020-01-02 17:40:00 +0200

    upgrade app structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './components/Header/Header';
+import ChildrenTable from './components/ChildrenTable/ChildrenTable';
+import Controllers from './components/Controllers/Controllers';
+import AddChildController from './components/Controllers/AddChildController';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Controllers>
+        <AddChildController />
+      </Controllers>
+      <ChildrenTable />
     </div>
   );
 }
