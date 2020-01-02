65295fae27058760e99254680a6968f9dfab28c7
Ziv Levy <levi.ziv@gmail.com>
2020-01-02 17:39:06 +0200

    add stick header on top

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">
+        Welcome to Gan Shosh!
+      </a>
+      <span class="badge badge-light">With us - your child gets the most</span>
+    </nav>
+  )
+}
+
+export default Header
