11c582000f1811d141fe72d6ee9b28ecb3eb9ac2
yaelz <yaelz@wix.com>
2020-01-09 13:47:58 +0200

    Children table showing from api without actions

--- src/components/ChildrenTable/ChildrenTable.js
+++ src/components/ChildrenTable/ChildrenTable.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ThemeContext from "../Providers/ThemeContext";
+import { API } from '../../database/api'
 
 const ChildrenTable = () => {
+    const [children, setChildren] = useState([])
+    useEffect ( () => {
+        async function fetchData() {
+            const childrenList = await API.getChildrenList()
+            setChildren(childrenList)
+        }
+
+        fetchData()
+    }, [])
   return (
       <ThemeContext.Consumer>
         {theme => (<table className={theme === 'light' ? "table table-light" : "table table-dark"}>
@@ -15,79 +25,46 @@ const ChildrenTable = () => {
                         </tr>
                       </thead>
                       <tbody>
-                        <tr>
-                          <th scope="row">1</th>
-                          <td>Yael Zaritsky</td>
-                          <td>
-                            <span className="badge badge-warning">Pending</span>
-                          </td>
-                          <td>
-                            <div className="dropdown">
-                              <button
-                                className="btn btn-secondary dropdown-toggle"
-                                type="button"
-                                id="dropdownMenuButton"
-                                data-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false"
-                              >
-                                Notify
-                              </button>
-                              <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                <a className="dropdown-item" href="#">
-                                    Notify parent for missing child
-                                </a>
-                                <a className="dropdown-item" href="#">
-                                    Notify day care staff
-                                </a>
-                                <a className="dropdown-item" href="#">
-                                    Notify general message
-                                </a>
-                              </div>
-                            </div>
-                                  </td>
-                                  <td>
-                                      <div className="btn-group" role="group">
-                                          <button type="button" className="btn btn-outline-success">Check In</button>
-                                          <button type="button" className="btn btn-outline-info">Sick</button>
+                        {children.map((child, index) => (
+                            <tr key={index}>
+                                <th key={index} scope="row">{index}</th>
+                                <td>{child.name}</td>
+                                <td>
+                                    <span className="badge badge-warning">Pending</span>
+                                </td>
+                                <td>
+                                    <div className="dropdown">
+                                        <button
+                                            className="btn btn-secondary dropdown-toggle"
+                                            type="button"
+                                            id="dropdownMenuButton"
+                                            data-toggle="dropdown"
+                                            aria-haspopup="true"
+                                            aria-expanded="false"
+                                        >
+                                            Notify
+                                        </button>
+                                        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                            <a className="dropdown-item" href="#">
+                                                Notify parent for missing child
+                                            </a>
+                                            <a className="dropdown-item" href="#">
+                                                Notify day care staff
+                                            </a>
+                                            <a className="dropdown-item" href="#">
+                                                Notify general message
+                                            </a>
+                                        </div>
                                     </div>
-                                  </td>
-                        </tr>
-                        <tr>
-                          <th scope="row">2</th>
-                          <td>Assaf Ganor</td>
-                          <td>
-                            <span className="badge badge-warning">Pending</span>
-                          </td>
-                        </tr>
-                        <tr>
-                          <th scope="row">3</th>
-                          <td>Ilona Shafir</td>
-                          <td>
-                            <span className="badge badge-warning">Pending</span>
-                          </td>
-                        </tr>
-                        <tr>
-                          <th scope="row">4</th>
-                          <td>Ziv Levy</td>
-                          <td>
-                            <span className="badge badge-success">OK</span>
-                          </td>
-                        </tr>
-                        <tr>
-                          <th scope="row">5</th>
-                          <td>Uri Avraham</td>
-                          <td>
-                            <span className="badge badge-danger">Missing</span>
-                          </td>
-                        </tr>
-                        <tr>
-                          <th scope="row">6</th>
-                          <td>Alon Gamzo</td>
-                          <td>
-                            <span className="badge badge-success">OK</span>
-                          </td>
-                        </tr>
+                                </td>
+                                <td>
+                                    <div className="btn-group" role="group">
+                                        <button type="button" className="btn btn-outline-success">Check In</button>
+                                        <button type="button" className="btn btn-outline-info">Sick</button>
+                                    </div>
+                                </td>
+                            </tr>
+                        ))}
                       </tbody>
                    </table>)}
       </ThemeContext.Consumer>
