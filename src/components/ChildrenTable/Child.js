7f7c2fb0d83dc4e321f408033a58d950f69e598e
yaelz <yaelz@wix.com>
2020-01-09 14:03:06 +0200

    Sick/checkedIn working

---
+++ src/components/ChildrenTable/Child.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react'
+import ThemeContext from "../Providers/ThemeContext";
+import { API } from '../../database/api'
+
+const Child = ({child, index}) => {
+  const [checkedIn, setCheckedIn] = useState(false)
+  const [sick, setSick] = useState(false)
+
+  return (<tr key={index}>
+            <th key={index} scope="row">{index}</th>
+            <td>{child.name}</td>
+            <td>
+                {!sick && !checkedIn && (<span className="badge badge-warning">Pending</span>)}
+                {sick && (<span className="badge badge-info">Sick</span>)}
+                {checkedIn && (<span className="badge badge-success">ok</span>)}
+            </td>
+            <td>
+                <div className="dropdown">
+                    <button
+                        className="btn btn-secondary dropdown-toggle"
+                        type="button"
+                        id="dropdownMenuButton"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                    >
+                        Notify
+                    </button>
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a className="dropdown-item" href="#">
+                            Notify parent for missing child
+                        </a>
+                        <a className="dropdown-item" href="#">
+                            Notify day care staff
+                        </a>
+                        <a className="dropdown-item" href="#">
+                            Notify general message
+                        </a>
+                    </div>
+                </div>
+            </td>
+            <td>
+                {(!checkedIn && !sick) ? (<div className="btn-group" role="group">
+                    <button type="button" className="btn btn-outline-success" onClick={() => setCheckedIn(true)} >Check In</button>
+                    <button type="button" className="btn btn-outline-info" onClick={() => setSick(true)} >Sick</button>
+                </div>) : new Date().toDateString()}
+            </td>
+        </tr>)
+}
+
+export default Child
--- src/components/ChildrenTable/ChildrenTable.js
+++ src/components/ChildrenTable/ChildrenTable.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import ThemeContext from "../Providers/ThemeContext";
 import { API } from '../../database/api'
+import Child from "./Child";
 
 const ChildrenTable = () => {
     const [children, setChildren] = useState([])
@@ -26,44 +27,7 @@ const ChildrenTable = () => {
                       </thead>
                       <tbody>
                         {children.map((child, index) => (
-                            <tr key={index}>
-                                <th key={index} scope="row">{index}</th>
-                                <td>{child.name}</td>
-                                <td>
-                                    <span className="badge badge-warning">Pending</span>
-                                </td>
-                                <td>
-                                    <div className="dropdown">
-                                        <button
-                                            className="btn btn-secondary dropdown-toggle"
-                                            type="button"
-                                            id="dropdownMenuButton"
-                                            data-toggle="dropdown"
-                                            aria-haspopup="true"
-                                            aria-expanded="false"
-                                        >
-                                            Notify
-                                        </button>
-                                        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                            <a className="dropdown-item" href="#">
-                                                Notify parent for missing child
-                                            </a>
-                                            <a className="dropdown-item" href="#">
-                                                Notify day care staff
-                                            </a>
-                                            <a className="dropdown-item" href="#">
-                                                Notify general message
-                                            </a>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div className="btn-group" role="group">
-                                        <button type="button" className="btn btn-outline-success">Check In</button>
-                                        <button type="button" className="btn btn-outline-info">Sick</button>
-                                    </div>
-                                </td>
-                            </tr>
+                            <Child child={child} index={index} key={index}/>
                         ))}
                       </tbody>
                    </table>)}
